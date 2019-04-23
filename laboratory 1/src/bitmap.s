3f404b9680b49b70ae0fd7c0668f6cf1bb86c145
rodrigo-xavier <rodrigomatiasxavier@gmail.com>
2019-04-23 20:29:18 -0300

    Alterações finais no bitmap.s

--- laboratory 1/src/bitmap.s
+++ laboratory 1/src/bitmap.s
@@ -15,12 +15,12 @@ FORA:	li t1, 0xFF000000		# endereco inicial da Memoria VGA
 		la s1, eagles		# endere�o dos dados da tela na memoria
 		addi s1, s1, 8		# primeiro pixels depois das informa��es de nlin ncol
 
-LOOP2: 	beq t1, t2, FIM			# Se for o �ltimo endere�o ent�o sai do loop
+LOOP: 	beq t1, t2, FIM			# Se for o �ltimo endere�o ent�o sai do loop
 		lw t3, 0(s1)		# le um conjunto de 4 pixels : word
 		sw t3, 0(t1)		# escreve a word na mem�ria VGA
 		addi t1, t1, 4		# soma 4 ao endere�o
 		addi s1, s1, 4
-		j LOOP2			# volta a verificar
+		j LOOP			# volta a verificar
 	
 # devolve o controle ao sistema operacional
 FIM:	li a7, 10			# syscall de exit
