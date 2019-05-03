c016ad149d01dd34b6c7805281c09240da9af0a5
ericvastro <eric.vcastro023@gmail.com>
2019-05-03 18:41:49 -0300

    Feito a questão Q3.3 e ja calculando a distância que tem de cada casa a casa

--- laboratory 1/Q3/1/pizzas.s
+++ laboratory 1/Q3/1/pizzas.s
@@ -1,17 +1,21 @@
 .data
 
-A: .string "Eric"
 N: .word 6 	# Numero de Casas/Clientes
 C: .space 160	# Espaço em bytes correspondente a 2 coordenadas x 20 casas (max) x 4 bytes, 
+D: .space 1600  # máximo de 20x20 casas x 4 bytes (float precisão simples)
+L1: .string "\n"
+L2: .string "   "
 
 
 .include "macros2.s"
 
 .text
-
+	la s2, D		# Inicio da matriz de distancias
 	la t0, N		#Le o endereço de N (clientes)
-	lw a0, 0(t0)		#Le o conteudo de N (clientes)
-	la a1, C		#le o endereçe de C (espaço dos endereços dos clientes)
+	lw s0, 0(t0)		#Le o conteudo de N (clientes)
+	la s1, C		#le o endereçe de C (espaço dos endereços dos clientes)
+	mv a0, s0
+	mv a1, s1
 	jal SORTEIO
 
 SORTEIO: 
@@ -79,13 +83,68 @@ TODASCASAS:	addi t0, t0, 64		# soma para dizer qual casa é, no ASCC
 		blt zero, t0,TODASCASAS	# Se não foram todos os clientes volta ao desenho dos endereços dos clientes
 		add t0, t0, a2		# Retornando a0 ao valor inicial
 		sub t1, t1, a3		# retorno para o inicio da pilha
-
-#---------------------------------------------------------------------------------------------------------------------------------	
+	
 			
-		mv a0, t0		#recupera os valores de a0 e a1
-		mv a1, t1
+		mv a0, s0		#recupera os valores de a0 e a1
+		mv a1, s1
+		jal ROTAS
 		
-		call FIM		# Vai para o fim	
+#----------------------------------------------------------------------------------------------------------------------------------		
+
+ROTAS:
+		mv t5, s2		# Pegando o endereço inicial de da Matriz
+		li t6, 999	 
+
+		mv t0, a0		# Guada o valor de a0 
+		mv t4, t0		# Inicia t4 com o mesmo valor de t0  
+		mv t1, a1		# Guada o valor de a1
+		mv t2, t1		# Apontando em t1 e em t2 o inicio da pilha 
+		addi t3, zero, 1	# Variavel de limite
+		jal LINHAPARAN
+
+LINHAPARAN:	beq t2, t1, SAOIGUAIS		# Verifica se os endereços de t0 e t1 são iguais, se são vão para SAO IGUAIS
+		lw a0, 0(t1)			# Posição X inicial   
+		lw a1, 4(t1)			# Posição Y inicial
+		lw a2, 0(t2)			# Posição X final
+		lw a3, 4(t2)			# Posição Y final
+		li a4, 0			# Cor da linha
+		li a5, 0			# Frame
+		li a7, 47			# Codigo de DrawLine
+		M_Ecall
+		
+		sub a0, a0, a2			# Variações de X do cliente atual e do proximo
+		sub a1, a1, a3			# Variações de Y do Cliente atual e do proximo
+		mul a0, a0, a0			# dX ao quadrado
+		mul a1, a1, a1			# dY ao quadrado
+		add a0, a0, a1			# Soma de dX e dY ao quadrado 
+		fcvt.s.w fa0,a0			# Converte o valor de a0 para Ponto Flutuante
+		fsqrt.s fa0,fa0 		# a raiz quadada da SOMA 
+				
+		fsw fa0, 0(t5)			# Guarda a distância entre os clientes na matriz D
+		addi t5, t5, 4			# Pula para a proxima posição da MATRIZ
+						
+		addi t0, t0, -1			# Diminui uma casa
+		addi t2, t2, 8			# Pula para o enereço do proximo cliente
+		bne t0, t3, LINHAPARAN		# Verifica se foram todos os clientes
+		bne t4, t3, PARATODASCASAS	# Verifica se foram todos os clientes 
+		
+		call FIM			# Vai para o fim
+
+PARATODASCASAS:
+		sub t4, t4, t3			# Pula para proximo cliente para ligar os endereçamentos comos outros clientes
+		mv t0, s0			# Retorna a quantidade de clientes total
+		mv t2, s1			# Retorna ao inicio da pilha
+		addi t1, t1, 8  		# Pula para o proximo Clinte a ligar os enedereços
+		jal LINHAPARAN			# Retorna ao endereçamento
+		
+
+SAOIGUAIS: 	
+		fcvt.s.w fa0, t6		# Se é iguais converte um numero muito grande para Ponto Flutuante
+		fsw fa0, 0(t5)			# Guarda a distância entre os clientes na matriz D
+		addi t5, t5, 4			# Pula para a proxima posição da MATRIZ
+		addi t2, t2, 8			# Se forem iguais o endereço t2 pula para proxima Casa
+		jal LINHAPARAN
+	
 
 
 FIM:
