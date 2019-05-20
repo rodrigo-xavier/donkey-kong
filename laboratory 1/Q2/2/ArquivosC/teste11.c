e7034c00630d7804b89932fec885497fb0ec71dd
rodrigo-xavier <rodrigomatiasxavier@gmail.com>
2019-05-20 20:51:35 -0300

    remove older

--- laboratory 1/Q2/2/ArquivosC/teste11.c
+++
@@ -1,7 +0,0 @@
-int soma_recursiva (int n) 
-{
-	if (n < 1)
-		return 0;
-	else
-		return n + soma_recursiva(n-1);
-}
--- laboratory 1/Q2/2/ArquivosC/teste2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	int i;
-	i=2047;  //2048 explique
-	
-	return i+i;		
-}
